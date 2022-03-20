80929073f3d8db3ad92380910fe441fb55251147
kabu <kabukunz@gmail.com>
2022-03-20 15:23:59 +0100

    more fixes

--- tutorial/505_Meshing/main.cpp
+++ tutorial/505_Meshing/main.cpp
@@ -22,10 +22,7 @@
 #include <igl/Timer.h>
 #include <string>
 
-#define NUM_N 1
-
 int N = 4;
-int currN = 0;
 Eigen::MatrixXi FMeshWhole, FMeshCut, FField, FSings, FSeams, FIso;
 Eigen::MatrixXd VMeshWhole, VMeshCut, VField, VSings, VSeams, VIso;
 Eigen::MatrixXd CField, CSeams, CSings, CIso;
@@ -38,75 +35,11 @@ Eigen::MatrixXi FPolyMesh;
 Eigen::MatrixXd VPolyMesh;
 Eigen::VectorXi singIndices, singVertices;
 Eigen::MatrixXd NFunction, NCornerFunction;
-
-// igl::opengl::glfw::Viewer viewer;
 directional::DirectionalViewer viewer;
+
 igl::Timer timer;
 std::string infilename, rawfieldname, outfilename;
 double elapsed;
-
-
-
-// #define NUM_N 3
-
-// int N[NUM_N];
-// int currN = 0;
-// Eigen::MatrixXi FMeshWhole, FMeshCut[NUM_N];
-// Eigen::MatrixXd VMeshWhole, VMeshCut[NUM_N];
-// Eigen::MatrixXd rawField[NUM_N], combedField[NUM_N];
-// Eigen::VectorXd effort[NUM_N], combedEffort[NUM_N];
-// Eigen::VectorXi matching[NUM_N], combedMatching[NUM_N];
-// Eigen::MatrixXi EV, FE, EF;
-// Eigen::VectorXi DPolyMesh[NUM_N];
-// Eigen::MatrixXi FPolyMesh[NUM_N];
-// Eigen::MatrixXd VPolyMesh[NUM_N];
-// Eigen::VectorXi singIndices[NUM_N], singVertices[NUM_N];
-// Eigen::MatrixXd NFunction[NUM_N], NCornerFunction[NUM_N];
-// directional::DirectionalViewer viewer;
-
-// typedef enum
-// {
-//     FIELD,
-//     INTEGRATION
-// } ViewingModes;
-// ViewingModes viewingMode = FIELD;
-
-// void update_raw_field_mesh()
-// {
-//     for (int i = 1; i <= 3; i++) //hide all other meshes
-//         viewer.data_list[i].show_faces = (viewingMode == FIELD);
-
-//     viewer.data_list[4].show_faces = (viewingMode == INTEGRATION);
-
-//     if (viewingMode == FIELD)
-//     {
-//         viewer.data_list[1].clear();
-//         viewer.data_list[1].set_mesh(VField, FField);
-//         viewer.data_list[1].set_colors(CField);
-//         viewer.data_list[1].show_faces = true;
-//         viewer.data_list[1].show_lines = false;
-
-//         viewer.data_list[2].clear();
-//         viewer.data_list[2].set_mesh(VSings, FSings);
-//         viewer.data_list[2].set_colors(CSings);
-//         viewer.data_list[2].show_faces = true;
-//         viewer.data_list[2].show_lines = false;
-
-//         viewer.data_list[3].clear();
-//         viewer.data_list[3].set_mesh(VSeams, FSeams);
-//         viewer.data_list[3].set_colors(CSeams);
-//         viewer.data_list[3].show_faces = true;
-//         viewer.data_list[3].show_lines = false;
-//     }
-//     else
-//     {
-//         viewer.data_list[4].clear();
-//         viewer.data_list[4].set_mesh(VIso, FIso);
-//         viewer.data_list[4].set_colors(CIso);
-//         viewer.data_list[4].show_faces = true;
-//         viewer.data_list[4].show_lines = false;
-//     }
-// }
 
 typedef enum
 {
@@ -150,29 +83,11 @@ bool key_down(igl::opengl::glfw::Viewer &viewer, int key, int modifiers)
     case '2':
         viewingMode = INTEGRATION;
         break;
-        // case '3': currN=(currN+1)%NUM_N; break;
     }
     update_viewer();
     return true;
 }
 
-// // Handle keyboard input
-// bool key_down(igl::opengl::glfw::Viewer &viewer, int key, int modifiers)
-// {
-//     switch (key)
-//     {
-//         // Select vector
-//     case '1':
-//         viewingMode = FIELD;
-//         break;
-//     case '2':
-//         viewingMode = INTEGRATION;
-//         break;
-//         // case '3': currN=(currN+1)%NUM_N; break;
-//     }
-//     update_raw_field_mesh();
-//     return true;
-// }
 
 int main(int argc, char *argv[])
 {
@@ -192,22 +107,20 @@ int main(int argc, char *argv[])
     std::string dataDir = std::string("/Users/max/Developer/Content/Data/");
 
     infilename = dataDir + cmdlineFile + ".off";
-    rawfieldname = dataDir + cmdlineFile + "-" + std::to_string(N) + ".rawfield";
-    outfilename = dataDir + cmdlineFile + "-" + std::to_string(N) + "-generated.off";
+    // rawfieldname = dataDir + cmdlineFile + "-" + std::to_string(N) + ".rawfield";
+    // outfilename = dataDir + cmdlineFile + "-" + std::to_string(N) + "-generated.off";
+    rawfieldname = dataDir + cmdlineFile + ".rawfield";
+    outfilename = dataDir + cmdlineFile + "-generated.off";
+
 
     igl::readOFF(infilename, VMeshWhole, FMeshWhole);
     directional::read_raw_field(rawfieldname, N, rawField);
     igl::edge_topology(VMeshWhole, FMeshWhole, EV, FE, EF);
 
     std::cout << "  1  Loaded field" << std::endl
-              << "  2  Show isoline mesh" << std::endl
-              << "  3  change between different N" << std::endl;
+              << "  2  Show isoline mesh" << std::endl;
 
     bool verbose = true;
-
-    // combing and cutting
-    //  for (int i = 0; i < NUM_N; i++)
-    //  {
 
     // TODO: rawfield generation
 
@@ -292,72 +205,6 @@ int main(int argc, char *argv[])
     viewer.set_singularities(singVertices, singIndices);
     viewer.set_seams(combedMatching);
     viewer.set_isolines(VMeshCut, FMeshCut, NFunction);
-
-    // //raw field mesh
-    // directional::glyph_lines_raw(VMeshWhole, FMeshWhole, combedField, directional::indexed_glyph_colors(combedField), VField, FField, CField, 1.0);
-
-    // if (i == 0)
-    // {
-    //     viewer.append_mesh();
-    //     viewer.data_list[1].clear();
-    //     viewer.data_list[1].set_mesh(VField, FField);
-    //     viewer.data_list[1].set_colors(CField);
-    //     viewer.data_list[1].show_faces = true;
-    //     viewer.data_list[1].show_lines = false;
-    // }
-
-    // //singularity mesh
-    // directional::singularity_spheres(VMeshWhole, FMeshWhole, N, singVertices, singIndices, VSings, FSings, CSings, 2.5);
-
-    // if (i == 0)
-    // {
-    //     viewer.append_mesh();
-    //     viewer.data_list[2].clear();
-    //     viewer.data_list[2].set_mesh(VSings, FSings);
-    //     viewer.data_list[2].set_colors(CSings);
-    //     viewer.data_list[2].show_faces = true;
-    //     viewer.data_list[2].show_lines = false;
-    // }
-
-    // //seams mesh
-    // Eigen::VectorXi isSeam = Eigen::VectorXi::Zero(EV.rows());
-    // for (int i = 0; i < FE.rows(); i++)
-    //     for (int j = 0; j < 3; j++)
-    //         if (intData.face2cut(i, j))
-    //             isSeam(FE(i, j)) = 1;
-    // directional::seam_lines(VMeshWhole, FMeshWhole, EV, combedMatching, VSeams, FSeams, CSeams, 2.5);
-
-    // if (i == 0)
-    // {
-    //     viewer.append_mesh();
-    //     viewer.data_list[3].clear();
-    //     viewer.data_list[3].set_mesh(VSeams, FSeams);
-    //     viewer.data_list[3].set_colors(CSeams);
-    //     viewer.data_list[3].show_faces = true;
-    //     viewer.data_list[3].show_lines = false;
-    // }
-
-    // directional::branched_isolines(VMeshCut, FMeshCut, NFunction, VIso, FIso, CIso);
-    // if (i == 0)
-    // {
-    //     viewer.append_mesh();
-    //     viewer.data_list[4].clear();
-    //     viewer.data_list[4].set_mesh(VIso, FIso);
-    //     viewer.data_list[4].set_colors(CIso);
-    //     viewer.data_list[4].set_face_based(true);
-    //     viewer.data_list[4].show_faces = false;
-    //     viewer.data_list[4].show_lines = false;
-    // }
-    // }
-
-    // viewer.data_list[0].clear();
-    // viewer.data_list[0].set_mesh(VMeshWhole, FMeshWhole);
-    // viewer.data_list[0].set_colors(directional::default_mesh_color());
-    // viewer.data_list[0].set_face_based(false);
-    // viewer.data_list[0].show_lines = false;
-    // update_raw_field_mesh();
-    // viewer.callback_key_down = &key_down;
-    // viewer.launch();
 
     update_viewer();
     viewer.callback_key_down = &key_down;
